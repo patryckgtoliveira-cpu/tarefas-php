6ec45612b111e7f604738798fb6f3fe38807716f
patryck geovany <patryckgtoliveira@gmail.com>
2025-11-27 19:51:06 -0300

    fiz um sitema de login

---
+++ home.php
@@ -0,0 +1,4 @@
+<?php
+    echo '<th2>bem vindo '.$session[login].'</th2>';
+    echo '<a hre"?logout">fazer logout!</a>';
+?>
--- index.php
+++ index.php
@@ -9,8 +9,33 @@
     <?php
 
         if(!isset($_SESSION['LOGIN'])){
+
+            if(isset($_session['login']))
+                
+                if(isset(@_post['acao'])){
+                    $login = 'patryck'; 
+                    $senha = '12345' 
+
+                    $loginform = $_post['login'];
+                    $senhaform = $_post['senha'];
+
+                    if($login == $loginform && $senha == $senhaform){
+                        //logado com sucesso  //posso colocar login no session , tambem funciona 
+                        $_session['loghin'] = true;
+                        header ('location: index.php');
+                    }else{
+                        //algum erro ocorreu
+                        echo 'dados invalidos.';
+                    }
+                }
+            
+
             include('login.php');
         }else{
+            if(isset($_get['logout'])){
+                unset($_session['login']);
+                header('location: index.php');
+            }
             include('home.php');
         }
     
