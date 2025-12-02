b5fc1c09c533dd1a0254802ad7ebe1538205bf18
patryck geovany <patryckgtoliveira@gmail.com>
2025-12-01 22:44:30 -0300

    corrigi alguns erros

--- index.php
+++ index.php
@@ -1,45 +1,34 @@
-<?php session_start() ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>login</title>
-</head>
-<body>
+<?php
+session_start();
 
-    <?php
+if(!isset($_SESSION['login'])){ 
+  
+        
+    if(isset($_POST['acao'])){
+        $login = 'patryck'; 
+        $senha = '12345'; 
 
-        if(!isset($_SESSION['LOGIN'])){
+        $loginform = $_POST['login'];
+        $senhaform = $_POST['senha'];
 
-            if(isset($_SESSION['login']))
-                
-                if(isset($_POST['acao'])){
-                    $login = 'patryck'; 
-                    $senha = '12345'; 
-
-                    $loginform = $_POST['login'];
-                    $senhaform = $_POST['senha'];
-
-                    if($login == $loginform && $senha == $senhaform){
-                        //logado com sucesso  //posso colocar login no session , tambem funciona 
-                        $_SEESION['loghin'] = true;
-                        header ('location: index.php');
-                    }else{
-                        //algum erro ocorreu
-                        echo 'dados invalidos.';
-                    }
-                }
-            
-
-            include('login.php');
+        if($login == $loginform && $senha == $senhaform){
+            //logado com sucesso  //posso colocar login no session , tambem funciona 
+            $_SESSION['login'] = true;
+            header ('location: index.php');
         }else{
-            if(isset($_GRT['logout'])){
-                unset($_SESSION['login']);
-                header('location: index.php');
-            }
-            include('home.php');
+            //algum erro ocorreu
+            echo 'dados invalidos.';
         }
+    }
     
-    ?>
 
-</body>
-</html>
+    include('login.php');
+}else{
+    if(isset($_GET['logout'])){
+        unset($_SESSION['login']);
+        header('location: index.php');
+    }
+    include('home.php');
+}
+
+?>
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <form method="post">
-    <imput type="text" name="login">
-    <imput type="passoword" name="senha">  
-    <imout type="submit" name="acao" velue="enviar!">
+    <input type="text" name="login">
+    <input type="passoword" name="senha">  
+    <input type="submit" name="acao" velue="enviar!">
 </form>
